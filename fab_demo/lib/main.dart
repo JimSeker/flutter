e6ce93d8e7e22fd121cf7a198952c0c8c948e3e8
Jim Ward <JimSeker@users.noreply.github.com>
2025-04-17 14:02:40 -0600

    moved m3 colors.

--- fab_demo/lib/main.dart
+++ fab_demo/lib/main.dart
@@ -20,7 +20,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.indigo, accentColor: Colors.pinkAccent, backgroundColor: Colors.cyan)
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.dark,
+        ),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -50,6 +53,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(child: Text(textToShow)),
