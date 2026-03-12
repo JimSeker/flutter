669fb32168650a5004b5d9081bd3a4c52ffc2b3b
Jim Ward <JimSeker@users.noreply.github.com>
2026-03-12 15:24:44 -0600

    updated libs and gradle.

--- speech_demo/windows/flutter/generated_plugin_registrant.cc
+++ speech_demo/windows/flutter/generated_plugin_registrant.cc
@@ -7,8 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <flutter_tts/flutter_tts_plugin.h>
+#include <speech_to_text_windows/speech_to_text_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FlutterTtsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterTtsPlugin"));
+  SpeechToTextWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("SpeechToTextWindows"));
 }
