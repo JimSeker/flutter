31eea8df6cb3286f5e23fb2af93f283673ba84a2
Jim Ward <JimSeker@users.noreply.github.com>
2026-03-13 08:39:34 -0600

    updated libs and gradle. verified works.

--- sqlitedb_demo/android/app/build.gradle.kts
+++ sqlitedb_demo/android/app/build.gradle.kts
@@ -8,15 +8,14 @@ plugins {
 android {
     namespace = "edu.cs4730.sqlitedb_demo"
     compileSdk = flutter.compileSdkVersion
-    //ndkVersion = flutter.ndkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
