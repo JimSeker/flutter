105aa3d2ed8ce8429ef4a3bb30cd2938ef4454fb
Jim Ward <JimSeker@users.noreply.github.com>
2026-03-13 09:56:27 -0600

    updated versions and tested.

--- flutter_mapdemo/android/app/build.gradle.kts
+++ flutter_mapdemo/android/app/build.gradle.kts
@@ -8,15 +8,15 @@ plugins {
 android {
     namespace = "edu.cs4730.flutter_mapdemo"
     compileSdk = flutter.compileSdkVersion
-    //ndkVersion = flutter.ndkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
+    //ndkVersion = "27.0.12077973"
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
@@ -24,8 +24,8 @@ android {
         applicationId = "edu.cs4730.flutter_mapdemo"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        //minSdk = flutter.minSdkVersion
-        minSdk = 24
+        minSdk = flutter.minSdkVersion
+        //minSdk = 26
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
