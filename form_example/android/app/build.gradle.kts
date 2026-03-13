0c7099e5d64bca85edc4487d04eae71b1f3ca596
Jim Ward <JimSeker@users.noreply.github.com>
2026-03-13 10:46:06 -0600

    reset the namespace correctly.

--- form_example/android/app/build.gradle.kts
+++ form_example/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.form_example"
+    namespace = "edu.cs4730.form_example"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.form_example"
+        applicationId = "edu.cs4730.form_example"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
